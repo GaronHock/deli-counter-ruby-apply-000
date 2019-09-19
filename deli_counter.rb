89195121313a16a4969f296553d390c0b215b177
wise-anchor-7998 <garonhock@yahoo.com>
2019-09-19 22:41:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,54 @@
 # Write your code here.
+
+
+def take_a_number(arr, str)
+arr.push(str)
+  i = arr.length  
+    puts "Welcome, " + arr[-1] + ". You are number " + i.to_s + " in line."
+end
+
+def line(arr)
+  if arr.length == 0 
+    puts "The line is currently empty."
+  end
+  i = 0 
+ 
+  string = arr.join(" ")
+  if arr.length > 0
+      puts "The line is currently: " + string
+    end
+end
+
+def now_serving(arr)
+  if arr.length == 0 
+    puts "There is nobody waiting to be served!"
+  end
+  if arr.length != 0
+ puts "Currently serving " + arr[0] + "."
+ arr.shift
+ end
+end  
+  
+
+
+katz_deli = []
+other_deli  = ["Logan", "Avi", "Spencer"] 
+another_deli =  ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] 
+  
+  puts line(katz_deli) 
+  
+    puts line(other_deli)
+  
+ puts line(another_deli) 
+  
+  puts take_a_number(katz_deli, "Ada") 
+   puts take_a_number(other_deli, "Grace")
+   
+   puts take_a_number(katz_deli, "Ada" )
+   puts take_a_number(katz_deli, "Grace")
+   puts take_a_number(katz_deli, "Kent") 
+   
+   puts now_serving(katz_deli)  
+  puts now_serving(other_deli)
+  
+
